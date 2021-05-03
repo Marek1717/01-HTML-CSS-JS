042074847e0be8ccb9094ff99589be4be5f2d103
Marek1717 <marekpisarczyk20@gmail.com>
2021-05-03 11:20:59 +0200

    First repository HTML CSS JS

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementById("me").onclick = function() {
+    if (document.getElementById("me").style.color == 'red')
+    document.getElementById("me").style.color = 'green'
+    else 
+    document.getElementById("me").style.color = 'red'
+}
+
